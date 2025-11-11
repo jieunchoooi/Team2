ab3fdaec249f8a58a2564d155a7219c1fc448889
Kim Hyunjin <ehgmlgg1216@gmail.com>
2025-11-11 16:50:36 +0900

    LectureVO 추가

--- Hobee/src/main/java/com/itwillbs/domain/LectureVO.java
+++ Hobee/src/main/java/com/itwillbs/domain/LectureVO.java
@@ -11,9 +11,18 @@ public class LectureVO {
 	
 	private int lecture_num;
     private String lecture_title;
+    private String lecture_author;
     private int lecture_price;
     private String category_detail;
     private int readcount;
     private String img_url;
+    private String lecture_img;
+    private String lecture_detail;
+    private String curriculum;
+    
+
+    	
+    	
+    	
 
 }
