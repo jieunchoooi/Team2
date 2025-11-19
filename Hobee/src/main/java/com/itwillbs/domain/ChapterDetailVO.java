e6a22b5a620453834a8dcd7b58745a157c80bf32
Kim Hyunjin <ehgmlgg1216@gmail.com>
2025-11-19 17:28:38 +0900

    강의 등록 커리큘럼 만드는중

--- Hobee/src/main/java/com/itwillbs/domain/ChapterDetailVO.java
+++ Hobee/src/main/java/com/itwillbs/domain/ChapterDetailVO.java
@@ -9,9 +9,9 @@ import lombok.ToString;
 @ToString
 public class ChapterDetailVO {
 	
-    private int detail_num;      // 고유넘버
-    private int chapter_num;     // chapter 테이블 fk
-    private int detail_order;    // 순서
+    private int detail_num;      // 고유넘버  --- 자동으로 올라가야함
+    private int chapter_num;     // chapter 테이블 fk  --- chaoterVO 고유넘버
+    private int detail_order;    // 순서   
     private String detail_title; // 제목
     private String detail_time;  // 강의시간
 
--- Hobee/src/main/java/com/itwillbs/domain/ChapterVO.java
+++ Hobee/src/main/java/com/itwillbs/domain/ChapterVO.java
@@ -11,9 +11,9 @@ import lombok.ToString;
 @ToString
 public class ChapterVO {
 	
-	  private int chapter_num;      // 고유넘버
+	  private int chapter_num;      // 고유넘버 
 	  private int lecture_num;      // lecture 테이블 fk
-	  private int chapter_order;    // 순서
+	  private int chapter_order;    // 순서 --- 자동으로 숫자 올라가야함
 	  private String chapter_title; // 제목
 	  
 	  private List<ChapterDetailVO> detailList;
