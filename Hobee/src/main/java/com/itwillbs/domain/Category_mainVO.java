3ba4ca0b8b0e107b953261bb08ce6c76bf1cb7fd
ehgml <ehgml@Yun>
2025-11-25 09:22:22 +0900

    카테고리 수정

---
+++ Hobee/src/main/java/com/itwillbs/domain/Category_mainVO.java
@@ -0,0 +1,15 @@
+package com.itwillbs.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+public class Category_mainVO {
+
+	private int category_main_num;
+	private String category_main_name;
+	
+}
